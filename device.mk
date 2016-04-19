5beb06561f588356c2126a8399f915d586ca8be8
Jose Adolfo Galdamez <josegalre@pac-rom.com>
2016-04-19 05:37:10 -0600

    kenzo: init.d: Add BSOD Killer
    * BSOD Killer - prevent ROM's black screen of death and data lose.
    
    Change-Id: I88264f7fe21327a93df0fb736282daa17ba01e6e
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_wcd9326.xml
 
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprint.msm8952
+
+# BSOD Killer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/etc/init.d/89bsod:system/etc/init.d/89bsod
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
