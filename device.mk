bded8826cea3941ee9b889407096c33ca4e6ace0
TheScarastic <warabhishek@gmail.com>
2017-01-22 06:06:43 +0000

    kenzo: Import oss camera hal from caf
     * LA.BR.1.3.3_rb2.26
    
    Change-Id: I327b443ce8276cf0a3fe785ea3338b4a8bcc097a

--- device.mk
+++ device.mk
@@ -26,9 +26,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_wcd9326.xml
 
 
+
 # BSOD Killer
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/init.d/89bsod:system/etc/init.d/89bsod
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8952 \
+    libqomx_core \
+    libmm-qcamera
+
 
 # Input
 PRODUCT_COPY_FILES += \
