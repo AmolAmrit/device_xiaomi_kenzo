83ee76d84c2400298b2105b97164f13d5f248bef
TheScarastic <warabhishek@gmail.com>
2017-08-15 18:32:14 +0530

    kenzo: Keep naming consistency across makefiles
    Change-Id: I4f58c22d857bcbae6b041fbd6d532592b6a3202f
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- device.mk
+++ device.mk
@@ -23,7 +23,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/kenzo/kenzo-vendor.mk)
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_wcd9326.xml
+    $(LOCAL_PATH)/audio/mixer_paths_wcd9326.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_wcd9326.xml
+
 
 
 # Fingerprint
