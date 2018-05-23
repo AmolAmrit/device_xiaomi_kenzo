03924994039801e3a42444433a3cfc9dba2701e1
AmolAmrit <amol.amrit03@outlook.com>
2018-05-23 14:58:04 +0530

    kenzo: Include SEPolicy
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_KERNEL_CONFIG := lineageos_kenzo_defconfig
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Sepolicy
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
